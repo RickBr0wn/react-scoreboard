b9faa88d0f30fcbc117f1ea62f330d894c858cd0
RiCK BROWN <ricky.brown.00@gmail.com>
2018-04-12 21:08:20 +0100

    updated comments

--- app.jsx
+++ app.jsx
@@ -1,3 +1,4 @@
+// ** Players Array ***********************
 var PLAYERS = [
   {
     name: "Jim Hoskins",
@@ -16,6 +17,42 @@ var PLAYERS = [
   },
 ];
 	
+// ** Add Players ***********************
+var AddPlayerForm = React.createClass({
+	propTypes: {
+		onAdd: React.PropTypes.func.isRequired,
+	},
+	
+	getInitialState: function(){
+		return {
+			name: "",
+		};
+	},
+	
+	onNameChange: function(e){
+		this.setState({name: e.target.value});
+	},
+	
+	onSubmit: function(e){
+		e.preventDefault();
+	
+		this.props.onAdd(this.state.name);
+		this.setState({name: ""});
+	},
+	
+	render: function(){
+		return(
+			<div className="add-player-form">
+				<form onSubmit={this.onSubmit}>
+					<input type="text" value={this.state.name} onChange={this.onNameChange} />
+					<input type="submit" value="Add Player" />
+				</form>
+			</div>
+		)
+	}
+});
+	
+// ** Stats ***********************
 function Stats(props){
 	var totalPlayers = props.players.length;
 	var totalPoints = props.players.reduce(function(total, player){
@@ -42,6 +79,7 @@ Stats.propTypes = {
 	players: React.PropTypes.array.isRequired,
 };
 
+// ** Header ***********************
 function Header(props) {
   return (
     <div className="header">
@@ -56,7 +94,7 @@ Header.propTypes = {
 	players: React.PropTypes.array.isRequired,
 };
 	
-function Counter(props){
+// ** Counter ***********************
 	return (
 		<div className="counter">
 			<button className="counter-action decrement" onClick={function(){
@@ -77,6 +115,7 @@ Counter.propTypes = {
 	onChange: React.PropTypes.func.isRequired,
 }
 
+// ** Player *********************** 
 function Player(props) {
   return (
     <div className="player">
@@ -96,6 +135,7 @@ Player.propTypes = {
 	onScoreChange: React.PropTypes.func.isRequired,
 };
 	
+// ** Application ***********************
 var Application = React.createClass({
 	propTypes: {
 		title: React.PropTypes.string,
@@ -140,6 +180,7 @@ var Application = React.createClass({
 						);
 					}.bind(this))}
 				</div>
+				<AddPlayerForm />
 			</div>
 		);
 	}
